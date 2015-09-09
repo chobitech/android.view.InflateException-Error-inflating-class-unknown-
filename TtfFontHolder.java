5d07db8fc249edb8e59cd53d25aa75a37960938a
Tadayoshi Fujiki <chobitech@users.noreply.github.com>
2015-09-09 13:49:45 +0900

    Create TtfFontHolder.java

---
+++ TtfFontHolder.java
@@ -0,0 +1,31 @@
+package com.chobitech.android.lib.components;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.HashMap;
+
+final class TtfFontHolder {
+
+	private final HashMap<String, Typeface> mFontMap = new HashMap<>();
+
+	private TtfFontHolder(){}
+
+	private static class TtfFontHolderHolder {
+		public static final TtfFontHolder INSTANCE = new TtfFontHolder();
+	}
+
+	public static TtfFontHolder getInstance() {
+		return TtfFontHolderHolder.INSTANCE;
+	}
+
+	public Typeface getTypeFace(Context context, String ttfName) {
+		if(mFontMap.containsKey(ttfName)) {
+			return mFontMap.get(ttfName);
+		} else {
+			Typeface tf = Typeface.createFromAsset(context.getAssets(), ttfName);
+			mFontMap.put(ttfName, tf);
+			return tf;
+		}
+	}
+}
