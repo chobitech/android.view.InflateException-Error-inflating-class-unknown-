adf090aaa4c68a53d7e6697d4b22deccffcab304
Tadayoshi Fujiki <chobitech@users.noreply.github.com>
2015-09-09 13:47:54 +0900

    Create TtfTextView.java

---
+++ TtfTextView.java
@@ -0,0 +1,46 @@
+package com.chobitech.android.lib.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.chobitech.android.lib.R;
+
+public class TtfTextView extends TextView {
+
+	private Context mContext;
+
+	public TtfTextView(Context context) {
+		super(context);
+		mContext = context;
+	}
+	public TtfTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+		init(attrs, R.styleable.TtfTextView_ttfTextViewFontFile);
+	}
+	public TtfTextView(Context context, AttributeSet attrs, int defStyleArrt) {
+		super(context, attrs, defStyleArrt);
+		mContext = context;
+		init(attrs, defStyleArrt);
+	}
+
+	private void init(AttributeSet attrs, int defStyleAttr) {
+		TypedArray arr = mContext.obtainStyledAttributes(attrs, R.styleable.TtfTextView, defStyleAttr, 0);
+		String ttfName = arr.getString(R.styleable.TtfTextView_ttfTextViewFontFile);
+		
+		/*
+		* the following code is a cause of the exception
+		*/
+		//setTypeface(Typeface.createFromAsset(mContext.getAssets(), ttfName));
+		
+		/*
+		* the fixed code with the class "TtfFontHolder, is a singleton and holds instances of TypeFace
+		*/
+		setTypeface(TtfFontHolder.getInstance().getTypeFace(mContext, ttfName));
+		
+		arr.recycle();
+	}
+}
